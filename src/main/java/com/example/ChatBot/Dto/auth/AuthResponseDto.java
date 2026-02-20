118d17ff2d07a8b484c84a9d725063c04e66d2d7
liril <liril.chandravanshi@univest.in>
2026-02-21 01:13:24 +0530

    feat: add DTO classes for authentication including AuthResponseDto, LoginDto, and SignupRequestDto

---
+++ src/main/java/com/example/ChatBot/Dto/auth/AuthResponseDto.java
@@ -0,0 +1,4 @@
+package com.example.ChatBot.Dto.auth;
+
+public class AuthResponseDto {
+}
---
+++ src/main/java/com/example/ChatBot/Dto/auth/SignupRequestDto.java
@@ -0,0 +1,30 @@
+package com.example.ChatBot.Dto;
+
+import jdk.jfr.DataAmount;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Getter
+@Setter
+
+public class SignupRequestDto implements Serializable {
+    @NotBlank(message = "Mobile is required")
+    @Pattern(regexp = "^[0-9]{10,15}$", message = "Invalid mobile number")
+    private String mobile;
+
+    @NotBlank(message = "Display name is required")
+    @Size(min = 2, max = 50)
+    private String displayName;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
